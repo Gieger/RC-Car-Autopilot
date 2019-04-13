bcc7f0d5af17294a37e5aeb2ed8bb1349edbda0a
Gieger <Gieger@live.de>
2019-04-13 18:13:15 +0200

    doku

--- docs/conf.py
+++ docs/conf.py
@@ -21,16 +21,14 @@ project = 'Herbie'
 copyright = '2019, Dennis Gieger'
 author = 'Dennis Gieger'
 
-# The full version, including alpha/beta/rc tags
-release = '2018'
-
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -45,7 +43,7 @@ language = 'de'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. Herbie documentation master file, created by
-   sphinx-quickstart on Sat Apr 13 17:47:08 2019.
+   sphinx-quickstart on Sat Apr 13 18:12:42 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
